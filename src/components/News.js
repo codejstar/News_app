d85eb7e1bf3390ae73f524bd75f9b161c6489b8b
code jstar <codejstar@gmail.com>
2024-08-20 17:12:36 +0530

    add fetch api and setup url

--- src/components/News.js
+++ src/components/News.js
@@ -4,28 +4,40 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
   static propTypes = {}
+  
+   constructor(){
+    //run before the rendering
+        super();
+        console.log("hello i am constructor from news components")
+        this.state= {
+           articles: [],
+           laoding: false
+        }
+      }
+    
+    async  componentDidMount(){
+        //run after render 
+        //life cycle method
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d874b971fc53477080733b6f75c7aaf8"
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+          articles: parsedData.articles
+        })
+      }
 
   render() {
+    //render after the constructor run
     return (
       <div className='container my-3'>
         <h2>Dhruv Rathee - Top Headlines</h2>
         <div className="row">
-        <div className="col-md-3">
-          <NewsItem title="Title" description="My description"/>
+        {this.state.articles.map((elem) => (
+        <div className="col-md-4" key={elem.url}>
+          <NewsItem title={elem.title.slice(0,45)} description={elem.description != null ? elem.description.slice(0,88) : elem.description} imgUrl={elem.urlToImage} newsUrl={elem.url}/>
         </div>
-       
-       <div className="col-md-4">
-          <NewsItem title="Title" description="My description"/>
-        </div>
-
-        <div className="col-md-4">
-          <NewsItem title="Title" description="My description"/>
-        </div>
-
-        <div className="col-md-4">
-          <NewsItem title="Title" description="My description"/>
-        </div>
-
+        ))}
         </div>
        </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,25 +2,28 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-    
+    //  constructor(){
+    //     super();
+    //     console.log("hello i am constructor")
+    //  }
     render() {
-      let {title, description} = this.props;
+      let {title, description,imgUrl,newsUrl} = this.props;
     return (
-      <>
+      <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={imgUrl == null ?"https://i0.wp.com/electrek.co/wp-content/uploads/sites/3/2024/08/Volkswagens-ID-GTI-2.jpeg?resize=1200%2C628&quality=82&strip=all&ssl=1":imgUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title">{title}...</h5>
         <p className="card-text">
-           {description}
+           {description}...
         </p>
-        <a href="#" className="btn btn-primary">
-         Go somewhere
+        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">
+          Read More
       </a>
   </div>
 </div>
 
-      </>
+      </div>
     )
   }
 }
